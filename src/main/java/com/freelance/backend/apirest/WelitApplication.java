8c1edbe73bf4493de5bafc785c410a71f6b49bc8
javier <javimayer14@gmail.com>
2020-07-19 16:45:35 -0300

    aaa

--- src/main/java/com/freelance/backend/apirest/WelitApplication.java
+++ src/main/java/com/freelance/backend/apirest/WelitApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class WelitApplication {
-
+  
 	public static void main(String[] args) {
 		SpringApplication.run(WelitApplication.class, args);
 
